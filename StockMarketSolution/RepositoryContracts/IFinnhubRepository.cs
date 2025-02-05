0b34825516f47c41a70829a217f532c9bffd2ef7
robert-braga <bragarobert99@gmail.com>
2025-02-05 08:10:49 +0200

    Add repository pattern + mock repository in tests

---
+++ StockMarketSolution/RepositoryContracts/IFinnhubRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryContracts
+{
+    /// <summary>
+    /// Repository interface for accessing data from the Finnhub API.
+    /// </summary>
+    public interface IFinnhubRepository
+    {
+        /// <summary>
+        /// Get the company profile for a stock symbol.
+        /// </summary>
+        /// <param name="stockSymbol">Stock symbol used to search the company profile</param>
+        /// <returns>Data about the company</returns>
+        Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol);
+
+        /// <summary>
+        /// Get the stock price quote for a stock symbol.
+        /// </summary>
+        /// <param name="stockSymbol">stock symbol to search</param>
+        /// <returns>Stock price quote for stock symbol</returns>
+        Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol);
+
+        /// <summary>
+        /// Get the stocks available in US.
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Dictionary<string, string>>?> GetStocks();
+
+        /// <summary>
+        /// Search for stocks based on the stock symbol.
+        /// </summary>
+        /// <param name="stockSymbolToSearch">Stock symbol to search</param>
+        /// <returns>Found stock based on given stock symbol</returns>
+        Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch);
+    }
+}
--- StockMarketSolution/Services/FinhubService.cs
+++ StockMarketSolution/Services/FinhubService.cs
@@ -3,82 +3,47 @@ using System;
 using System.Net.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json; // Add this using directive
+using Newtonsoft.Json;
+using RepositoryContracts; // Add this using directive
 
 
 namespace Services
 {
     public class FinhubService : IFinnhubService
     {
-        private readonly IHttpClientFactory _httpClientFactory; // Complete the declaration
-        private readonly IConfiguration _configuration;
+        private readonly IFinnhubRepository _finnhubRepository;
 
-        public FinhubService(IHttpClientFactory httpClientFactory, IConfiguration configuration) // Add constructor to initialize IHttpClientFactory
+        public FinhubService(IFinnhubRepository finnhubRepository)
         {
-            _httpClientFactory = httpClientFactory;
-            _configuration = configuration;
+            _finnhubRepository = finnhubRepository;
         }
 
         public async Task<Dictionary<string,object>>? GetCompanyProfile(string stockSymbol)
         {
-            HttpClient client = _httpClientFactory.CreateClient();
+            var responseDictionary = await _finnhubRepository.GetCompanyProfile(stockSymbol);
 
-            client.DefaultRequestHeaders.Add("X-Finnhub-Token", _configuration["finhubToken"]);
-
-            HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://finnhub.io/api/v1/stock/profile2?symbol={stockSymbol}")
-            };
-
-            var response = await client.SendAsync(httpRequestMessage);
-
-            var content = await response.Content.ReadAsStringAsync();
-
-            var responsDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(content);
-
-            if (responsDictionary == null)
-            {
-                throw new InvalidOperationException($"No response from the server.");
-            }
-
-            if (responsDictionary.ContainsKey("error"))
-            {
-                throw new InvalidOperationException($"Error: {responsDictionary["error"]}");
-            }
-
-            return responsDictionary;
+            return responseDictionary;
         }
 
         public async Task<Dictionary<string, object>>? GetStockPriceQuote(string stockSymbol)
         {
-            HttpClient client = _httpClientFactory.CreateClient();
+            var responseDictionary = await _finnhubRepository.GetStockPriceQuote(stockSymbol);  
 
-            client.DefaultRequestHeaders.Add("X-Finnhub-Token", _configuration["finhubToken"]);
+            return responseDictionary;
+        }
 
-            HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}")
-            };
+        public async Task<List<Dictionary<string, string>>?> GetStocks()
+        {
+            var response = await _finnhubRepository.GetStocks();
 
-            var response = await client.SendAsync(httpRequestMessage);
+            return response;
+        }
 
-            var content = await response.Content.ReadAsStringAsync();
+        public async Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch)
+        {
+            var responseDictionary = await _finnhubRepository.SearchStocks(stockSymbolToSearch);
 
-            var responsDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(content);
-
-            if (responsDictionary == null)
-            {
-                throw new InvalidOperationException($"No response from the server.");
-            }
-
-            if (responsDictionary.ContainsKey("error"))
-            {
-                throw new InvalidOperationException($"Error: {responsDictionary["error"]}");
-            }
-
-            return responsDictionary;
+            return responseDictionary;
         }
     }
 }
